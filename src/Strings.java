23db530a0f280b10d2f7bec8d7bba42e636585d2
NITIN SINGH <111533570+Nitinkharayatt@users.noreply.github.com>
2023-01-04 17:06:38 +0530

    javastring

--- src/Strings.java
+++ src/Strings.java
@@ -1,15 +1,20 @@
 public class Strings {
 
-    public static boolean repeated(String str) {
-        String s = str + str;
-        return s.substring(0, s.length() - 1).contains(str);
-
-    }
+//    public static boolean repeated(String str) {
+//        String s = str + str;
+//        return s.substring(0, s.length() - 1).contains(str);
+//
+//    }
     public static void main(String[] args) {
         String str="aba";
-        System.out.println(repeated(str));
+         StringBuilder sb=new StringBuilder("");
+         for (char ch='a'; ch<='z';ch++){
+             sb.append(ch);
+         }
+        System.out.println(sb);
 
 //        String s=name+name;
+
 //        System.out.println(s.length());
 //        System.out.println(s);
 //        System.out.println(name.substring(2,5));
